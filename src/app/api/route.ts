cbf9bb34ee3cea8865510bca50bc22e759fb2902
donjar <mail@herbert.id>
2024-01-18 18:09:25 +0800

    empty

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -2,12 +2,7 @@ import { NextRequest } from "next/server";
 import { Client } from "pg";
 
 export const POST = async (req: NextRequest) => {
-  const client = new Client({
-    host: "localhost",
-    database: "3ic",
-    user: "readonly",
-    password: "readonly",
-  });
+  const client = new Client({});
   await client.connect();
 
   const res = await client.query(...((await req.json()) as [string, any[]]));
